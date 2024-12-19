d9c1eaaff4bd9eef4d19a63f6170dc3b85fd420e
MIneth03 <minethsandew@gmail.com>
2024-12-19 07:56:01 +0530

    remove unwanted codes

--- Preprocessing/Formatting.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-file_path = 'C:/Users/asus/OneDrive/Documents/GitHub/Client-Predicting/Dataset/bank-full.csv' 
-data = pd.read_csv(file_path, delimiter=';')
-
-output_path = 'C:/Users/asus/OneDrive/Documents/GitHub/Client-Predicting/Dataset/DatasetML.csv' 
-data.to_csv(output_path, index=False)
-
-print(data.head())
